081882aa51b74b45d88f70c6b72f71252c02e83a
Maxwell Seifert <silverhammer04@yahoo.com>
2020-08-31 09:30:45 -0600

    First Heroku Deployment

---
+++ public/precache-manifest.6ece0f2e01b9609f96e0cad11b96c932.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08bb5251235d0fed15e4825b5a9613b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f2f461f8419dd75a9a2",
+    "url": "/static/css/2.aa9c7ed0.chunk.css"
+  },
+  {
+    "revision": "44e1a589a53232a87a16",
+    "url": "/static/css/main.4376443e.chunk.css"
+  },
+  {
+    "revision": "5f2f461f8419dd75a9a2",
+    "url": "/static/js/2.51a12c3f.chunk.js"
+  },
+  {
+    "revision": "bd80297531dafeefc287ffe4d2830fea",
+    "url": "/static/js/2.51a12c3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44e1a589a53232a87a16",
+    "url": "/static/js/main.0a3dde4d.chunk.js"
+  },
+  {
+    "revision": "1aadcde5ef86e7287682",
+    "url": "/static/js/runtime-main.44d2f03b.js"
+  }
+]);
